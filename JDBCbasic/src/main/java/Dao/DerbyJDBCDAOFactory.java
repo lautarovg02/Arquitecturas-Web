40bb16d8610dd304eededed5aadeec3957ccdb11
lautarovg02 <lautarovg147@gmail.com>
2023-09-21 09:47:12 -0300

    New: Trabajando con JPA y HIBERNATE

--- JDBCbasic/src/main/java/Dao/DerbyJDBCDAOFactory.java
+++ JDBCbasic/src/main/java/Dao/DerbyJDBCDAOFactory.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static Dao.MySqlJDBCDAOFactory.createConnection;
+
 public class DerbyJDBCDAOFactory extends DAOFactory {
 
     @Override
