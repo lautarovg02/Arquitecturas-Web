fb830a69bcb52c7ccd2c0e8fc440f7ec79bcf750
lautarovg02 <lautarovg147@gmail.com>
2023-09-21 11:23:24 -0300

    Fix: agregando ejemplo listas de personas

---
+++ JPA-BASIC/src/main/java/Select.java
@@ -0,0 +1,26 @@
+import dao.Persona;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class Select {
+    public static void main(String[] args) {
+        //CONEXION A LA BD
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Example");
+        // le decimso a emf que ahora si nso cree un EntityManager
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        //Le pedimos que busque una persona atraves de su clave primaria
+        Persona j = em.find(Persona.class, 1);// el 1 seria el id
+        System.out.println(j);
+        //Consulta para traer todas las personas
+        @SuppressWarnings("unchecked")
+        List<Persona> personas = em.createQuery("SELECT p FROM Persona p").getResultList();
+        personas.forEach(p-> System.out.println(p));
+        //CERRANDO LA CONEXION
+        em.getTransaction().commit();
+        //CERRAMOS EL "EntityManager" QUE ESTABAMOS USANDO
+        em.close();
+        emf.close();
+    }
+}
